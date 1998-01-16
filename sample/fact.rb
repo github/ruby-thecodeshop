c40c4186eee33ec44c23eb0fcc07d126d3990b21
(no author) <(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
1998-01-16 12:13:08 +0000

    This commit was manufactured by cvs2svn to create branch 'KOSAKO'.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/KOSAKO@4 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- sample/fact.rb
+++
@@ -1,8 +0,0 @@
-def fact(n)
-  if n == 0
-    1
-  else
-    n * fact(n-1)
-  end
-end
-print fact(ARGV[0].to_i), "\n"
--- version.h
+++
@@ -1,2 +0,0 @@
-#define RUBY_VERSION "1.0-971225"
-#define VERSION_DATE "97/12/25"
