005008112d4029d68019bfdca0d16a183923ce9c
James Golick <jamesgolick@gmail.com>
2013-04-09 23:43:16 -0700

    we don't need to clear a method cache tree if we're gc'ing a class - its subclasses are unreachable now anyway

--- gc.c
+++ gc.c
@@ -2491,7 +2491,6 @@ obj_free(rb_objspace_t *objspace, VALUE obj)
 	break;
       case T_MODULE:
       case T_CLASS:
-	rb_clear_cache_by_class((VALUE)obj);
 	rb_free_m_table(RCLASS_M_TBL(obj));
 	if (RCLASS_IV_TBL(obj)) {
 	    st_free_table(RCLASS_IV_TBL(obj));
