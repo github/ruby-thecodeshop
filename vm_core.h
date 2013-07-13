22ff3a1c8cab9abe118a3800f67e05b587805222
Charlie Somerville <charlie@charliesomerville.com>
2013-07-13 02:22:46 -0700

    add ic_ptr field to IC

--- vm_core.h
+++ vm_core.h
@@ -126,8 +126,9 @@ struct iseq_compile_data_ensure_node_stack;
 typedef struct rb_compile_option_struct rb_compile_option_t;
 
 struct iseq_inline_cache_entry {
-    VALUE ic_vmstat;
-    VALUE ic_seq;
+    uint64_t ic_vmstat;
+    uint64_t ic_seq;
+    uint64_t* ic_ptr;
     VALUE ic_class;
     union {
 	VALUE value;
