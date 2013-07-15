aade8cec2ca749de229b4bffd9b7ffae3c68a376
Charlie Somerville <charlie@charliesomerville.com>
2013-07-14 20:18:23 -0700

    rename definemethod and definesingletonmethod coz that shits too long

--- compile.c
+++ compile.c
@@ -4641,7 +4641,7 @@ iseq_compile_each(rb_iseq_t *iseq, LINK_ANCHOR *ret, NODE * node, int poped)
 
 	debugp_param("defn/iseq", iseqval);
 
-	ADD_INSN2(ret, nd_line(node), definemethod, ID2SYM(node->nd_mid), iseqval);
+	ADD_INSN2(ret, nd_line(node), defn, ID2SYM(node->nd_mid), iseqval);
 
 	if (!poped) {
 	    ADD_INSN(ret, nd_line(node), putnil);
@@ -4658,7 +4658,7 @@ iseq_compile_each(rb_iseq_t *iseq, LINK_ANCHOR *ret, NODE * node, int poped)
 	debugp_param("defs/iseq", iseqval);
 
 	COMPILE(ret, "defs: recv", node->nd_recv);
-	ADD_INSN2(ret, nd_line(node), definesingletonmethod, ID2SYM(node->nd_mid), iseqval);
+	ADD_INSN2(ret, nd_line(node), defs, ID2SYM(node->nd_mid), iseqval);
 
 	if (!poped) {
 	    ADD_INSN(ret, nd_line(node), putnil);
