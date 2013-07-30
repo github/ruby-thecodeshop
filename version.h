af4d97209732b40ca67f1206dee0c012e8e593ab
Charlie Somerville <charlie@charliesomerville.com>
2013-07-30 16:29:54 -0700

    little thing to test chatops ruby build

--- version.h
+++ version.h
@@ -33,10 +33,10 @@
 # ifdef RUBY_BRANCH_NAME
 #  define RUBY_REVISION_STR " "RUBY_BRANCH_NAME" "STRINGIZE(RUBY_REVISION)
 # else
-#  define RUBY_REVISION_STR ", TCS patched 2012-05-27, GitHub v1.0.32"
+#  define RUBY_REVISION_STR ", TCS patched 2012-05-27, GitHub v1.0.32 - chatops ruby build test"
 # endif
 #else
-# define RUBY_REVISION_STR ", TCS patched 2012-05-27, GitHub v1.0.32"
+# define RUBY_REVISION_STR ", TCS patched 2012-05-27, GitHub v1.0.32 - chatops ruby build test"
 #endif
 
 # define RUBY_DESCRIPTION	    \
